b91b5e2dde0f3c8f9e8642621844079899450abd
kait-mar <rim.bakkali63@gmail.com>
2021-12-12 22:48:55 +0100

    parsing

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 typedef struct lst
 {
     char    *cmd;
-    char    *arg;
+    char    **arg;
     char    meta;
-    t_lst   *next;
+    struct lst   *next;
 }   t_lst;
 char    *my_substr(char *str, char a, char b)
 {
@@ -45,44 +46,127 @@ char    *ft_strdup(char *str)
 
 t_lst   *init_lst(t_lst *lst)
 {
-    lst->arg = NULL;
+    //lst->arg = NULL;
     lst->cmd= NULL;
     lst->meta = 0;
+    lst->next = NULL;
     return (lst);
 }
 
-int    parse_error(char **argv, t_lst **cmd)
+t_lst    *parse_error(char **argv)
 {
     t_lst   *temp;
+    t_lst   *head;
     int     i = 1;
+    int check = 0;
+    int     j;
 
+    head = malloc(sizeof(t_lst));
+    head = init_lst(head);
+    head->arg = malloc(sizeof(char *) *  1000);
+        head->cmd = ft_strdup(argv[i++]);
+    printf("B\n");
+    if (strcmp(argv[i], ";") == 0)
+    {
+        head->meta = ';';
+        i++;
+    }
+    else if (strcmp(argv[i], "|") == 0)
+    {
+        head->meta = '|';
+        i++;
+    }
+    else
+    {
+        j = 0;
+        while (argv[i] && !strcmp(argv[i], ";") && !strcmp(argv[i], "|"))
+        {
+                            printf("argv = %s\n", argv[i]);
+            head->arg[j++] = ft_strdup(argv[i++]);
+        }
+    }
+    printf("Asdd\n");
+    temp = head;
+       temp->next = malloc(sizeof(t_lst));
+        temp->next = init_lst(temp->next);
+        temp->next->arg = malloc(sizeof(char *) * 100);
+        temp = temp->next;
     while (argv[i])
     {
-        temp = malloc(sizeof(t_lst));
-        temp = init_lst(temp);
-        temp->cmd = ft_strdup(argv[i++]);
+        if (check != 1)
+            temp->cmd = ft_strdup(argv[i++]);
         if (strcmp(argv[i], ";") == 0)
         {
+            check = 0;
             temp->meta = ';';
+            printf("Enter 1\n");
+
+            temp->next = malloc(sizeof(t_lst));
+            temp->next = init_lst(temp->next);
+            temp->next->arg = malloc(sizeof(char *) *  1000);
+            temp = temp->next;
             i++;
         }
         else if (strcmp(argv[i], "|") == 0)
         {
+            printf("Enter2 \n");
+            check = 0;
             temp->meta = '|';
+            temp->next = malloc(sizeof(t_lst));
+            temp->next = init_lst(temp->next);
+            temp->next->arg = malloc(sizeof(char *) *  1000);
+            temp = temp->next;
             i++;
         }
         else
-            temp->arg = ft_strdup(argv[i++]);
+        {
+            j = 0;
+            printf("Enter \n");
+                printf("argv = %s\n", argv[i]);
+            while (argv[i] &&  strcmp(argv[i], ";") != 0 && strcmp(argv[i], "|") != 0)
+            { 
+                printf("argc    v = %s\n", argv[i]);
+                temp->arg[j++] = ft_strdup(argv[i++]);
+                printf("ééé\n");
+            }
+            printf("End\n");
+            check = 1;
+        }
     }
+    temp->next = NULL;
+    return (head);
 }
 
-int main(int argc, char **argv, char **env)
+void    print_lst(t_lst *lst)
 {
-    t_lst    *cmd;
+    t_lst   *temp = lst;
+    while (temp != NULL)
+    {
+        printf("cmd ==> %s\n", temp->cmd);
+        printf("arg ==> %s\n", temp->arg[0]);
+        printf("meta ==>; %c\n", temp->meta);
+        temp = temp->next;
+    }
+}
+ int main(int argc, char **argv, char **env)
+//int main()
+{
+    t_lst    *lst;
+    // int     argc = 7;
+    // char    **argv;
+    // argv[1] = "/bin/ls";
+    // argv[2] = "|";
+    // argv[3] = "usr/bin/ls";
+    // argv[4] = "usr/bin/grep";
+    // argv[5] = "microshell";
+    // argv[6] = NULL;
 
     if (argc == 1)
         return (0);
-    cmd = malloc(sizeof(t_lst));
-    parse_error(argv[1], &cmd);
-    //this wouldn't compile, just put the general structure!
+    printf("Before\n");
+    lst = parse_error(argv);
+    printf("A\n");
+    print_lst(lst);
+    return (0);
 }
+;
